25ebbaa16c7c709415470215a145c7bc9d93eb86
James <Jameshoggard.dev@gmail.com>
2021-12-30 13:51:30 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
-
+import { FaSearch, FaShoppingCart, FaUserCircle } from 'react-icons/fa';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="Header">
+        <div>
+          <div className="Logo"> Amazon Clone </div>
+        </div>
+        <div className="Search">
+          <input type="Text" placeholder="Search Product"></input>
+          <div className="Search_icon">
+            <FaSearch size="20px" />
+          </div>
+        </div>
+        <div className="Icons">
+          <div><FaShoppingCart color="white" size="25px" /></div>
+          <div><FaUserCircle color="white" size="25px" /></div>
+        </div>
+      </div>
+      <div className="Categories">
+        <div>Mobile</div>
+        <div>Laptop</div>
+        <div>Monitor</div>
+        <div>Computer Accessories</div>
+        <div>Earphones</div>
+      </div>
+      <p></p>
+      <Carousel
+      additionalTransfrom={0}
+      arrows
+      autoPlaySpeed={3000}
+      centerMode={false}
+      className=""
+      draggable
+      focusOnSelect={false}
+        infinite
+        itemClass=''
+        keyBoardControl
+        minimumTouchDrag={80}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          desktop:{
+            breakpoint:{
+              max: 3000,
+              min: 1024
+            },
+            items: 1
+          },
+          mobile: {
+            breakpoint: {
+              max: 3000,
+              min: 1024
+            },
+            items: 1
+          },
+          tablet: {
+            breakpoint: {
+              max:3000,
+              min: 1024
+            },
+            items: 1
+          },
+        }}
+        showDots
+        sliderClass=''
+        slidesToSlide={1}
+        swipeable
         >
-          Learn React
-        </a>
-      </header>
+        <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_WC_186x116_Evergreen_Oct_2021._SY116_CB640520999_.jpg"
+          style={{
+            display: "block",
+            height: "30rem",
+            margin: "auto",
+            width:"100%",
+            objectFit:"fill",  
+          }}
+          alt=""
+          />
+        <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_WC_186x116_Evergreen_Oct_2021._SY116_CB640520999_.jpg"
+          style={{
+            display: "block",
+            height: "30rem",
+            margin: "auto",
+            width: "100%",
+            objectFit: "fill",
+          }}
+          alt=""
+        />
+        <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_WC_186x116_Evergreen_Oct_2021._SY116_CB640520999_.jpg"
+          style={{
+            display: "block",
+            height: "30rem",
+            margin: "auto",
+            width: "100%",
+            objectFit: "fill",
+          }}
+          alt=""
+        />
+        <img src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonServices/Site/US/Product/FBA/Outlet/Merchandising/Outlet_GW_WC_186x116_Evergreen_Oct_2021._SY116_CB640520999_.jpg"
+          style={{
+            display: "block",
+            height: "30rem",
+            margin: "auto",
+            width: "100%",
+            objectFit: "fill",
+          }}
+          alt=""
+        />
+        </Carousel>
+        <p></p>
+        <br></br>
+        <p></p>
+        <Home/>
     </div>
   );
 }
 
-export default App;
+export default App
+
+
---
+++ src/Screen/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+  return (
+    <div className="Home">
+      <div className="Pro">
+          Products
+      </div>
+      <main className="main">
+          <div className="content">
+              <ul className="products">
+                <li>
+                    <div className="product">-
+                        <img className="product-image" src="">
+                    </div>
+                </li>
+              </ul>
+          </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home
